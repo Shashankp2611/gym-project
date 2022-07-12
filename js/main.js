bec4fdaaf281a8867bfc51903c6703ca018a39e2
Shashankp2611 <104302597+Shashankp2611@users.noreply.github.com>
2022-07-12 10:19:54 +0530

    final

--- js/main.js
+++ js/main.js
@@ -1,174 +1,110 @@
-/*  ---------------------------------------------------
-  Template Name: Gym
-  Description:  Gym Fitness HTML Template
-  Author: Colorlib
-  Author URI: https://colorlib.com
+/* ===================================
+--------------------------------------
+  X Gym - Fitness HTML Template
   Version: 1.0
-  Created: Colorlib
----------------------------------------------------------  */
+--------------------------------------
+======================================*/
+
 
 'use strict';
 
-(function ($) {
+$(window).on('load', function() {
+	/*------------------
+		Preloder
+	--------------------*/
+	$(".loader").fadeOut();
+	$("#preloder").delay(400).fadeOut("slow");
 
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
-    });
+});
 
-    /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
-
-    //Canvas Menu
-    $(".canvas-open").on('click', function () {
-        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
-        $(".offcanvas-menu-overlay").addClass("active");
-    });
-
-    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
-        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
-        $(".offcanvas-menu-overlay").removeClass("active");
-    });
-
-    // Search model
-    $('.search-switch').on('click', function () {
-        $('.search-model').fadeIn(400);
-    });
-
-    $('.search-close-switch').on('click', function () {
-        $('.search-model').fadeOut(400, function () {
-            $('#search-input').val('');
-        });
-    });
-
-    //Masonary
-    $('.gallery').masonry({
-        itemSelector: '.gs-item',
-        columnWidth: '.grid-sizer',
-        gutter: 10
-    });
-
-    /*------------------
+(function($) {
+	/*------------------
 		Navigation
 	--------------------*/
-    $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
+	$(".main-menu").slicknav({
+        appendTo: '.header-section',
         allowParentLinks: true
     });
 
-    /*------------------
-        Carousel Slider
-    --------------------*/
-    var hero_s = $(".hs-slider");
-    hero_s.owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: true,
-        items: 1,
-        dots: false,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: false
-    });
+	/*------------------
+		Background Set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
 
-    /*------------------
-        Team Slider
-    --------------------*/
-    $(".ts-slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 3,
-        dots: true,
-        dotsEach: 2,
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-        responsive: {
-            320: {
-                items: 1,
-            },
-            768: {
-                items: 2,
-            },
-            992: {
-                items: 3,
-            }
-        }
-    });
+	/*------------------
+		BMI calculator
+	--------------------*/
+	function  computeBMI () {
+		// user inputs
+		var height = Number(document.getElementById("bmi-hight").value);
+		var weight = Number(document.getElementById("bmi-weight").value);
+		var result = weight / (height * height);
 
-    /*------------------
-        Testimonial Slider
-    --------------------*/
-    $(".ts_slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 1,
-        dots: false,
-        nav: true,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true
-    });
+		//Display result of calculation
+		var output = Math.round(result * 100) / 100;
+		if (output < 18.5) {
+			document.getElementById("bmi-result").value = output + " (Underweight)";
+		} else if (output >= 18.5 && output <= 25) {
+			document.getElementById("bmi-result").value = output + " (Normal)";
+		} else if (output >= 25 && output <= 30) {
+			document.getElementById("bmi-result").value = output + " (Obese)";
+		}else if (output > 30) {
+			document.getElementById("bmi-result").value = output + " (Overweight)";
+		} else {
+			document.getElementById("bmi-result").value = output; 
+		}
+	}
 
-    /*------------------
-        Image Popup
-    --------------------*/
-    $('.image-popup').magnificPopup({
-        type: 'image'
-    });
+	$('#bmi-submit').on('click', function(){
+		computeBMI();
+	});
 
-    /*------------------
-        Video Popup
-    --------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
 
-    /*------------------
-        Barfiller
-    --------------------*/
-    $('#bar1').barfiller({
-        barColor: '#ffffff',
-        duration: 2000
-    });
-    $('#bar2').barfiller({
-        barColor: '#ffffff',
-        duration: 2000
-    });
-    $('#bar3').barfiller({
-        barColor: '#ffffff',
-        duration: 2000
-    });
+	/*------------------
+		Accordions
+	--------------------*/
+	$('.panel-link').on('click', function (e) {
+		$('.panel-link').removeClass('active');
+		var $this = $(this);
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+		e.preventDefault();
+	});
 
-    $('.table-controls ul li').on('click', function () {
-        var tsfilter = $(this).data('tsfilter');
-        $('.table-controls ul li').removeClass('active');
-        $(this).addClass('active');
 
-        if (tsfilter == 'all') {
-            $('.class-timetable').removeClass('filtering');
-            $('.ts-meta').removeClass('show');
-        } else {
-            $('.class-timetable').addClass('filtering');
-        }
-        $('.ts-meta').each(function () {
-            $(this).removeClass('show');
-            if ($(this).data('tsmeta') == tsfilter) {
-                $(this).addClass('show');
-            }
-        });
-    });
+	/*------------------
+		Circle progress
+	--------------------*/
+	$('.circle-progress').each(function() {
+		var cpvalue = $(this).data("cpvalue");
+		var cpcolor = $(this).data("cpcolor");
+		var cptitle = $(this).data("cptitle");
+		var cpid 	= $(this).data("cpid");
+
+		$(this).append('<div class="'+ cpid +'"></div><div class="progress-info"><h2>'+ cpvalue +'%</h2><p>'+ cptitle +'</p></div>');
+
+		if (cpvalue < 100) {
+
+			$('.' + cpid).circleProgress({
+				value: '0.' + cpvalue,
+				size: 211,
+				thickness: 5,
+				fill: cpcolor,
+				emptyFill: "rgba(0, 0, 0, 0)"
+			});
+		} else {
+			$('.' + cpid).circleProgress({
+				value: 1,
+				size: 211,
+				thickness: 5,
+				fill: cpcolor,
+				emptyFill: "rgba(0, 0, 0, 0)"
+			});
+		}
+	});
 
 })(jQuery);
